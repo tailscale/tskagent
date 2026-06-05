a9e6d66482f3145008651b8c2c385b57490978df
Andrew Dunham <andrew@tailscale.com>
2026-06-05 17:51:03 -0400

    Add test for parseComment
    Signed-off-by: Andrew Dunham <andrew@tailscale.com>

--- internal_test.go
+++ internal_test.go
@@ -4,8 +4,14 @@
 package tskagent
 
 import (
+	"crypto"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/pem"
 	"testing"
 
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/agent"
 
 	_ "embed"
@@ -33,5 +39,70 @@ func TestKeyParse(t *testing.T) {
 	}
 	if got, want := key.Signer.PublicKey().Type(), "ssh-ed25519"; got != want {
 		t.Errorf("Key type: got %q, want %q", got, want)
+	}
+}
+
+func TestParseComment(t *testing.T) {
+	type genKeyFunc func(t *testing.T) crypto.PrivateKey
+
+	runTest := func(t *testing.T, genKey genKeyFunc, comment string) {
+		priv := genKey(t)
+
+		privPem, err := ssh.MarshalPrivateKey(priv, comment)
+		if err != nil {
+			t.Fatalf("MarshalPrivateKey: %v", err)
+		}
+
+		pemText := pem.EncodeToMemory(privPem)
+
+		// We expect exactly empty or "comment-here", nothing else.
+		switch got := parseComment(pemText); got {
+		case "":
+			//t.Logf("got empty comment")
+		case "comment-here":
+			//t.Logf("got comment: %q", got)
+		default:
+			t.Fatalf("got unexpected comment: %q", got)
+		}
+	}
+
+	const iterations = 1_000
+
+	tests := []struct {
+		name   string
+		genKey genKeyFunc
+	}{
+		{
+			name: "ed25519",
+			genKey: func(t *testing.T) crypto.PrivateKey {
+				_, priv, err := ed25519.GenerateKey(nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				return priv
+			},
+		},
+		{
+			name: "rsa",
+			genKey: func(t *testing.T) crypto.PrivateKey {
+				key, err := rsa.GenerateKey(rand.Reader, 2048)
+				if err != nil {
+					t.Fatal(err)
+				}
+				return key
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name+"_no_comment", func(t *testing.T) {
+			for i := 0; i < iterations; i++ {
+				runTest(t, tt.genKey, "")
+			}
+		})
+		t.Run(tt.name+"_comment", func(t *testing.T) {
+			for i := 0; i < iterations; i++ {
+				runTest(t, tt.genKey, "comment-here")
+			}
+		})
 	}
 }
