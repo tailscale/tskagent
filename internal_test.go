9c5cdbb6ab8194788f45317ee6b2f5b7e3a1e9d0
M. J. Fromberger <fromberger@tailscale.com>
2026-06-05 18:43:58 -0700

    tskagent: fix incorrect comment parsing from key files (#1)
    The code to pull comments out of key data made some heuristic (but incorrect)
    assumptions about the organization of the key. In particular, for keys where no
    comment is defined, it could incorrectly grab an earlier field (e.g., one of
    the key parameters) which would confuse a client expecting printable text.
    Correctly process the fields we care about, so that when an empty comment is
    set we don't grab the wrong thing by mistake.
    
    Also, update the tests to cover RSA as well as ED25519 keys, and generate test
    keys randomly. I ran the new tests 50,000 times under "stress" and was not able
    to produce any new failures.
    
    Thanks to @andrew-d for reporting this and for providing a failing test to
    debug from.
    
    Co-authored-by: Andrew Dunham <andrew@tailscale.com>

--- internal_test.go
+++ internal_test.go
@@ -4,34 +4,85 @@
 package tskagent
 
 import (
+	"crypto"
+	"crypto/ed25519"
+	crand "crypto/rand"
+	"crypto/rsa"
+	"encoding/pem"
 	"testing"
 
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/agent"
-
-	_ "embed"
 )
 
 var _ agent.Agent = &Server{}
 
-// The test data key is a throwaway generated for testing, and is not used
-// anywhere else.  To generate a new test key, run:
-//
-//    ssh-keygen -C "Dummy key for testing" -t ed25519 -f testdata/test.key
-
-//go:embed testdata/test.key
-var testPrivKey []byte
-
 func TestKeyParse(t *testing.T) {
-	key, err := parseStoredKey("foo", 1, testPrivKey)
-	if err != nil {
-		t.Fatalf("Parsing stored key: %v", err)
+	tests := []struct {
+		name    string
+		input   []byte
+		comment string
+		keyType string
+	}{
+		{
+			name:    "ED2559/Comment",
+			input:   mustGenerateKey(t, genED25519, "elliptic justice"),
+			comment: "elliptic justice",
+			keyType: "ssh-ed25519",
+		},
+		{
+			name:    "ED2559/NoComment",
+			input:   mustGenerateKey(t, genED25519, ""),
+			comment: "",
+			keyType: "ssh-ed25519",
+		},
+		{
+			name:    "RSA/Comment",
+			input:   mustGenerateKey(t, genRSA, "what year is it"),
+			comment: "what year is it",
+			keyType: "ssh-rsa",
+		},
+		{
+			name:    "RSA/NoComment",
+			input:   mustGenerateKey(t, genRSA, ""),
+			comment: "",
+			keyType: "ssh-rsa",
+		},
 	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			key, err := parseStoredKey(tc.name, 1, tc.input)
+			if err != nil {
+				t.Fatalf("parsing stored key: %v", err)
+			}
+			if key.Comment != tc.comment {
+				t.Errorf("Comment: got %q, want %q", key.Comment, tc.comment)
+			}
+			if got := key.Signer.PublicKey().Type(); got != tc.keyType {
+				t.Errorf("Key type: got %q, want %q", got, tc.keyType)
+			}
+		})
+	}
+}
 
-	const wantComment = "Dummy key for testing"
-	if key.Comment != wantComment {
-		t.Errorf("Comment: got %q, want %q", key.Comment, wantComment)
+func mustGenerateKey(t *testing.T, gen func() (crypto.PrivateKey, error), comment string) []byte {
+	t.Helper()
+	key, err := gen()
+	if err != nil {
+		t.Fatalf("Generating key: %v", err)
 	}
-	if got, want := key.Signer.PublicKey().Type(), "ssh-ed25519"; got != want {
-		t.Errorf("Key type: got %q, want %q", got, want)
+	enc, err := ssh.MarshalPrivateKey(key, comment)
+	if err != nil {
+		t.Fatalf("Marshaling key: %v", err)
 	}
+	return pem.EncodeToMemory(enc)
+}
+
+func genED25519() (crypto.PrivateKey, error) {
+	_, key, err := ed25519.GenerateKey(crand.Reader)
+	return key, err
+}
+
+func genRSA() (crypto.PrivateKey, error) {
+	return rsa.GenerateKey(crand.Reader, 1024)
 }
